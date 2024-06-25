a832d368e75ce8db63ca4ca136e9f3b0c6c3eb74
Venkata Ratnam Bhumireddy <58448762+venkataratnamb20@users.noreply.github.com>
2024-06-26 00:03:55 +0200

    updated project.v
    connected sarlogic inside the project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,17 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out[7:5] = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+  // wire _unused = &{ena, clk, rst_n, 1'b0};
+    sarlogic(
+        .clk(clk), 
+        .reset(~rst_n), 
+        .d(ui_in[0]),
+        .bitout(uo_out[3:0]),
+        .conv_done(uo_out[4]);
 endmodule
