1cd7d3778d31e7e8f12daf59f27af2cd9bb7a115
Venkata Ratnam Bhumireddy <58448762+venkataratnamb20@users.noreply.github.com>
2024-06-26 01:23:05 +0200

    Fix test and added more assertions.
    Fix test and added more assertions.
    after 1clock: 0
    after 2clock: 8
    after 3clock: 4

--- test/test.py
+++ test/test.py
@@ -34,7 +34,20 @@ async def test_project(dut):
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 8
+    assert dut.uo_out.value == 0
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 8
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 4
