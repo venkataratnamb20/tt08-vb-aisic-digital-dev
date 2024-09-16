6f887b6a593fb6ab7b27bcbead3607e8c7f9109f
Venkata Ratnam Bhumireddy <58448762+venkataratnamb20@users.noreply.github.com>
2024-09-16 12:31:40 +0000

    project.v: uio_out is configured properly by assigning to 0 to avoid 'z' error.

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_example (
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   //  assign uo_out[7:5] = 0;
-  assign uio_out[7:5] = 0;
+  assign uio_out[7:4] = 0;
   assign uio_oe  = 0;
 
   wire [11:0] bitout_w;
